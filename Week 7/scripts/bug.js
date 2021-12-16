d606cc1b4530e1b232197b1e8c0c1bf86e47adb9
krosentrater <87054047+krosentrater@users.noreply.github.com>
2021-12-15 21:06:57 -0800

    Capstone Project
    Bug Reporter

--- Week 7/scripts/bug.js
+++ Week 7/scripts/bug.js
@@ -8,45 +8,63 @@ class Bug {
         this.bugDesc = arguments[3]
     };
 
-    addBug() {
+    addBug() { 
         bugTracker ++;
+        
         let newBug = '<div id="bug' + bugTracker + '" class="bugRpt"><p>';
             newBug += 'Reported by: ' + this.reportedBy + '/' + this.subSystem + '<br/>';
             newBug += 'System: ' + this.system + '/' + this.subSystem + '<br />';
             newBug += this.bugDesc + '</p>';
-            newBug += '<div class="bugButtons">'
-                newBug += '<button onClick="objBug.deleteBug(' + bugTracker + ')" class="bugBttn">X</button>';
-                newBug += '<button onClick="objBug.resolveBug(' + bugTracker + ')" class="bugBttn">&#x2713;</button>'
+            newBug += `<div id='bugButtons${bugTracker}' class='bugButtons'`;
             newBug += '</div>';
         newBug += '</div>';
 
         document.querySelector('#listWrapper').innerHTML += newBug;
+
+        let bugButtonsDiv = document.getElementById('bugButtons' + bugTracker);
+
+        let deleteButton = document.createElement('button');
+        deleteButton.innerHTML = 'X';
+        deleteButton.id = 'deleteMe-' + bugTracker;
+        deleteButton.addEventListener('click', this.deleteBug);
+        bugButtonsDiv.appendChild(deleteButton);
+
+        let resolveButton = document.createElement('button');
+        resolveButton.innerHTML = '&#x2713;';
+        resolveButton.id = 'resolveMe-' + bugTracker;
+        resolveButton.addEventListener('click', this.resolveBug);
+        bugButtonsDiv.appendChild(resolveButton);
+        console.dir(deleteButton);
     };
 
-    deleteBug(bugTracker) {
-        let element = document.getElementById('bug' + bugTracker);
+    deleteBug(event) {
+        let getId = event.target.id;
+        let id = parseInt(getId.substring(9));
+        console.log(id)
+        let element = document.getElementById('bug'+id);
         element.remove();
     };
     
 
-    resolveBug(bugTracker) {
-        let element = document.getElementById('bug' + bugTracker);
+    resolveBug(event) {
+        let getId = event.target.id;
+        let id = parseInt(getId.substring(10));
+        console.log(id)
+        let element = document.getElementById('bug' + id);
         element.style.backgroundColor = '#111111';
         element.style.color = 'silver';
     };
 };
 
 function reportBug() {
-    
+    console.log('asdf')
     let objBug = new Bug (
         document.querySelector('#reportedBy').value,
         document.querySelector('#system').selectedOptions[0].text,
         document.querySelector('#subSystem').selectedOptions[0].text,
         document.querySelector('#bugDesc').value,
-        console.log(objBug)
     )
     objBug.addBug();
-    console.log(objBug)
 };
 
 document.getElementById('bugForm').addEventListener('submit', reportBug);
